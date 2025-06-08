5a46ac47b41d552721440f488e04fecfce5027e8
Success Aje <73680210+successaje@users.noreply.github.com>
2025-06-08 17:17:54 +0100

    fix: bars duplicates, chore: theme mode

--- src/components/providers/ClientProviders.tsx
+++ src/components/providers/ClientProviders.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { ThemeProvider } from '@/context/theme-context';
 import { Web3Providers } from './Web3Providers';
+import { ThemeProvider } from '@/context/theme-context';
 import { Toaster } from '@/components/ui/Toaster';
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider>
-      <Web3Providers>
+    <Web3Providers>
+      <ThemeProvider>
         {children}
         <Toaster />
-      </Web3Providers>
-    </ThemeProvider>
+      </ThemeProvider>
+    </Web3Providers>
   );
 }
